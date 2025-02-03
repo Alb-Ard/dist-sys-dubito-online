d79ac72027507ecbdb67c573da957841c69a45b1
Alberto Arduini <albertoa679@gmail.com>
2025-02-03 21:34:29 +0100

    Added junit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,17 @@ plugins {
     id("application")
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
 
 application {
