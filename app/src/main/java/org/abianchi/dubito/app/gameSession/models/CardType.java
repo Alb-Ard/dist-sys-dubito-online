a57ff988f967913a884b967eb6c31009022d2312
Alberto Arduini <albertoa679@gmail.com>
2025-07-02 21:57:43 +0200

    Fixed CardType serialization

--- app/src/main/java/org/abianchi/dubito/app/gameSession/models/CardType.java
+++ app/src/main/java/org/abianchi/dubito/app/gameSession/models/CardType.java
@@ -1,20 +1,16 @@
 package org.abianchi.dubito.app.gameSession.models;
 
-import java.util.Random;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum CardType {
-    QUEEN_OF_HEARTS(CardValue.QUEEN),
-    QUEEN_OF_SPADES(CardValue.QUEEN),
-    KING_OF_HEARTS(CardValue.KING),
-    KING_OF_SPADES(CardValue.KING),
-    ACE_OF_SPADES(CardValue.ACE),
-    ACE_OF_HEARTS(CardValue.ACE),
-    JOKER(CardValue.JOKER);
-
+    QUEEN_OF_HEARTS(CardValue.QUEEN), QUEEN_OF_SPADES(CardValue.QUEEN), KING_OF_HEARTS(CardValue.KING),
+    KING_OF_SPADES(CardValue.KING), ACE_OF_SPADES(CardValue.ACE), ACE_OF_HEARTS(CardValue.ACE), JOKER(CardValue.JOKER);
 
     private final CardValue value;
 
-    private CardType(CardValue value) {
+    @JsonCreator
+    private CardType(@JsonProperty("value") final CardValue value) {
         this.value = value;
     }
 
