9a960b32d877a6d8e05fe06fad435d293acb86dd
Alberto Arduini <albertoa679@gmail.com>
2025-07-11 01:57:19 +0200

    Fixed concurrent message sending in SerialMessageSender

--- app/src/main/java/org/albard/dubito/messaging/SerialMessageSender.java
+++ app/src/main/java/org/albard/dubito/messaging/SerialMessageSender.java
@@ -1,14 +1,17 @@
 package org.albard.dubito.messaging;
 
-import java.io.IOException;
 import java.io.OutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Function;
 
 import org.albard.dubito.messaging.messages.GameMessage;
+import org.albard.utils.Logger;
 
 public final class SerialMessageSender implements MessageSender {
     private final OutputStream stream;
     private final Function<GameMessage, byte[]> serializer;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     private SerialMessageSender(final OutputStream stream, final Function<GameMessage, byte[]> serializer) {
         this.stream = stream;
@@ -23,10 +26,19 @@ public final class SerialMessageSender implements MessageSender {
     @Override
     public void sendMessage(final GameMessage message) {
         try {
-            this.stream.write(this.serializer.apply(message));
-            this.stream.flush();
-        } catch (final IOException ex) {
-            ex.printStackTrace();
+            executor.submit(() -> {
+                try {
+                    final var data = this.serializer.apply(message);
+                    Logger.logTrace("Writing " + data.length + "b...");
+                    this.stream.write(data);
+                    this.stream.flush();
+                    Logger.logTrace("Written " + data.length + "b");
+                } catch (final Exception ex) {
+                    Logger.logError("Could not send message: " + ex.getMessage());
+                }
+            }).get();
+        } catch (final Exception ex) {
+            Logger.logError("Error waiting in sendMessage: " + ex.getMessage());
         }
     }
 }
